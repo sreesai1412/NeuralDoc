c1b5e8142f682ccdf1599b912b70f025f94dff37
harsha <sreesai1412@gmail.com>
2020-10-17 20:24:00 +0530

    Added all

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import os
+import time
+
+
+def predict(message):
+    #os.system("cd scripts")
+    f = open("./data/python/sample.code", "w")
+    f.write(message)
+    f.close()
+    print("Wrote code to file" + message)
+    os.system("bash ./scripts/generate.sh -1 code2jdoc sample.code")
+    # time.sleep(10)
+    f = open("pred.txt", "r")
+    doc = f.read()
+    f.close()
+    return doc
+
+
+st.title("AutoDoc")
+st.subheader("NITK, Surathkal")
+st.header("CS305 Software Engineering Mini Project")
+st.subheader("Sai Sree Harsha (181CO146) & Aditya Sohoni (181CO203)")
+st.title("Automating Code Documentation using Machine Learning")
+message = st.text_area("Enter Code Below", height=350)
+if st.button("Analyze"):
+    with st.spinner("Analyzing the text …"):
+        prediction = predict(message)
+        st.success(prediction)

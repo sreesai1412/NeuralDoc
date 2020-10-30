ae0399c8498bc2af81244545d0e180a229978914
Sai Sree Harsha <sreesai1412@gmail.com>
2020-10-30 08:20:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,20 @@ import re
 def preprocess(message):
     return message.translate({ord(c): None for c in '.,\'\":;)(!\n\t'})
 
+def _max_width_():
+    max_width_str = f"max-width: 1300px;"
+    st.markdown(
+        f"""
+    <style>
+    .reportview-container .main .block-container{{
+        {max_width_str}
+    }}
+    </style>
+    """,
+        unsafe_allow_html=True,
+    )
+
+
 def predict(message):
     #os.system("cd scripts")
     f = open("./data/python/sample.code", "w")
@@ -20,14 +34,39 @@ def predict(message):
     return doc
 
 
-st.title("AutoDoc")
-st.subheader("NITK, Surathkal")
-st.header("CS305 Software Engineering Mini Project")
+def predict2(message):
+    #os.system("cd scripts")
+    f = open("./data/java/sample.code", "w")
+    f.write(message)
+    f.close()
+    print("Wrote code to file" + message)
+    os.system("bash ./scripts/generate2.sh -1 code2jdoc sample.code")
+    # time.sleep(10)
+    f = open("pred.txt", "r")
+    doc = f.read()
+    f.close()
+    return doc
+
+
+_max_width_()
+st.title("NeuralDoc | Automating Code Documentation using Machine Learning")
+st.header("CS305 Software Engineering Mini Project, NITK, Surathkal")
 st.subheader("Sai Sree Harsha (181CO146) & Aditya Sohoni (181CO203)")
-st.title("Automating Code Documentation using Machine Learning")
-message = st.text_area("Enter Code Below", height=350)
-message = preprocess(message)
-if st.button("Analyze"):
-    with st.spinner("Analyzing the text …"):
-        prediction = predict(message)
-        st.success(prediction)
+#st.write("Documentation of source code is a tedious process, but nevertheless an integral part of software development. Our system, NeuralDoc automatically generates documentation for Python and Java methods at the click of a button. NeuralDoc leverages cutting edge techniques such as the Transformer and BERT to gain a deep understanding of the code and generate a high quality documentation. NeuralDoc boosts developer productivity and renders the documentation process completely painless.")
+
+col1, col2, col3 = st.beta_columns([4, 1, 4])
+
+with col1:
+    message = st.text_area("Enter Python Code Below", height=350)
+    if st.button("Analyze", key='py'):
+        with st.spinner("Documenting Code, Please Wait ......."):
+            prediction = predict(message)
+            st.success(prediction)
+
+with col3:
+    message = st.text_area("Enter Java Code Below", height=350)
+    if st.button("Analyze", key='java'):
+        with st.spinner("Documenting Code, Please Wait ......."):
+            prediction = predict2(message)
+            st.success(prediction)
+
